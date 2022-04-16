95fbe8abb4162ec9fe25bd3d92058ca3a00e363f
Yasir <code.yasir@gmail.com>
2022-04-16 08:46:54 +0300

    add alert component logic

--- src/Alert.js
+++ src/Alert.js
@@ -1,7 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 
-const Alert = () => {
-  return <h2>alert component</h2>
-}
+const Alert = ({ success, text, removeAlert, list }) => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      //invoking without params becuase of the default params
+      removeAlert();
+    }, 3000);
 
-export default Alert
+    //added the list depenedency to create new timeout on every re-render
+    return () => clearTimeout(timeout);
+  }, [list]);
+  return (
+    <p className={`alert ${success ? "alert-success" : "alert-danger"}`}>
+      {text}
+    </p>
+  );
+};
+
+export default Alert;
