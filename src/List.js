d9bd3e373e5b4fd0862187f5795914358d3728e8
Yasir <code.yasir@gmail.com>
2022-04-16 08:45:39 +0300

    add list component logic

--- src/List.js
+++ src/List.js
@@ -1,7 +1,27 @@
-import React from 'react'
-import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
-}
+import React from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
+const List = ({ item, handleRemove, handleEdit }) => {
+  return (
+    <div className="grocery-item">
+      <p className="title">{item}</p>
+      <div>
+        <button
+          type="button"
+          className="edit-btn"
+          onClick={() => handleEdit(item)}
+        >
+          <FaEdit />
+        </button>
+        <button
+          type="button"
+          className="delete-btn"
+          onClick={() => handleRemove(item)}
+        >
+          <FaTrash />
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default List
+export default List;
